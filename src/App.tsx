d1bb526df7223fd151249c7a0303846ff8532afb
JoshBarrette <joshpbarrette@gmail.com>
2024-11-19 18:27:53 -0600

    Fetch

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Landing from "./components/Landing";
 import NavBar from "./components/NavBar";
 
 function App() {
-  fetch("/api")
+  fetch("https://josh.barrette.dev/api")
     .then((res) => console.log(res.body))
     .catch((e) => console.log(e));
 
